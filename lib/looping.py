2cf63ade67848f2b9fe86a96e7d0e8608559ec88
dkiriinya <108690877+dkiriinya@users.noreply.github.com>
2024-02-27 10:24:37 +0300

    loops

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,33 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    i = 10
+    while (i>0):
+        print(i)
+        i-= 1
+    print("Happy New Year!")
+happy_new_year()
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    squared_list = []
+    for num in int_list:
+        squared_num = num * num
+        squared_list.append(squared_num)
+
+    return squared_list
+result = square_integers([1, 2, 3, 4, 5])
+print(result)
+
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for i in range(1,101):
+        if (i % 3 == 0 and i % 5 == 0):
+            print ('FizzBuzz')
+        elif (i % 3 == 0):
+            print('Fizz')
+        elif (i % 5 == 0):
+            print ("Buzz")
+        else:
+            print(i)
+fizzbuzz()
+
